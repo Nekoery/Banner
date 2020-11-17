bbc6a161525189b7e70aa5f6af6b39f3356046bd
Nekoery <iorzqaq@gmail.com>
2020-11-17 12:56:19 +0800

    @第三次提交

--- app/src/main/java/com/test/banner/MainActivity.java
+++ app/src/main/java/com/test/banner/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.LinearLayout;
 import java.util.Timer;
 import java.util.TimerTask;
 
-//第二次提交
+//第三次提交
 public class MainActivity extends AppCompatActivity{
     AdapterViewFlipper flipper;
     LinearLayout main;
